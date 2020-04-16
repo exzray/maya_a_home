b7972a744733b68c7760501300e58153c019ffee
pytho <pythorevolution@gmail.com>
2020-04-16 09:11:16 +0800

    initial commit

--- app/src/main/java/com/afiq/myapplication/MainAdminActivity.java
+++ app/src/main/java/com/afiq/myapplication/MainAdminActivity.java
@@ -1,16 +1,40 @@
 package com.afiq.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.afiq.myapplication.adapters.AdminMainAdapter;
+import com.afiq.myapplication.databinding.ActivityMainAdminBinding;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
+
 public class MainAdminActivity extends AppCompatActivity {
+
+    private ActivityMainAdminBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_admin);
+        binding = ActivityMainAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setTitle("Maya A Home Admin");
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+        AHBottomNavigationAdapter navigationAdapter = new AHBottomNavigationAdapter(this, R.menu.option_admin_main);
+        navigationAdapter.setupWithBottomNavigation(binding.navigation);
+
+        binding.pager.setAdapter(new AdminMainAdapter(getSupportFragmentManager()));
+        binding.navigation.setAccentColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        binding.navigation.setOnTabSelectedListener(this::tabSelectedListener);
+    }
+
+    private boolean tabSelectedListener(int position, boolean wasSelected) {
+        binding.pager.setCurrentItem(position, true);
+        return true;
     }
 }
---
+++ app/src/main/java/com/afiq/myapplication/adapters/AdminMainAdapter.java
@@ -0,0 +1,42 @@
+package com.afiq.myapplication.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.afiq.myapplication.fragments.AdminEnquireFragment;
+import com.afiq.myapplication.fragments.AdminPaymentFragment;
+import com.afiq.myapplication.fragments.AdminProjectFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminMainAdapter extends FragmentStatePagerAdapter {
+
+    private static final List<Fragment> FRAGMENTS = new ArrayList<>();
+
+
+    public AdminMainAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+
+        AdminProjectFragment fragment1 = new AdminProjectFragment();
+        AdminPaymentFragment fragment2 = new AdminPaymentFragment();
+        AdminEnquireFragment fragment3 = new AdminEnquireFragment();
+
+        FRAGMENTS.add(fragment1);
+        FRAGMENTS.add(fragment2);
+        FRAGMENTS.add(fragment3);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return FRAGMENTS.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return FRAGMENTS.size();
+    }
+}
