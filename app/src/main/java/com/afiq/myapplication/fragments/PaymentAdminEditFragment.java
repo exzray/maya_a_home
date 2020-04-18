ea198256fa4581a2648848b0320c5e8401d400aa
pytho <pythorevolution@gmail.com>
2020-04-18 10:28:09 +0800

    initial commit

---
+++ app/src/main/java/com/afiq/myapplication/fragments/PaymentAdminEditFragment.java
@@ -0,0 +1,29 @@
+package com.afiq.myapplication.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.afiq.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PaymentAdminEditFragment extends Fragment {
+
+    public PaymentAdminEditFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment_admin_edit, container, false);
+    }
+}
