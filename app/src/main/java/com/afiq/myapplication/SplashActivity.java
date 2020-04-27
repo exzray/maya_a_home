83c0f763c261baf5134bef5d2567d3412dac0d6c
pytho <pythorevolution@gmail.com>
2020-04-27 15:07:30 +0800

    initial commit

--- app/src/main/java/com/afiq/myapplication/SplashActivity.java
+++ app/src/main/java/com/afiq/myapplication/SplashActivity.java
@@ -1,20 +1,22 @@
 package com.afiq.myapplication;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.afiq.myapplication.databinding.ActivitySplashBinding;
 import com.afiq.myapplication.models.ProfileModel;
 import com.afiq.myapplication.utilities.Database;
 import com.afiq.myapplication.utilities.Interaction;
+import com.afiq.myapplication.viewmodels.ProfileViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.roger.catloadinglibrary.CatLoadingView;
 
 public class SplashActivity extends AppCompatActivity {
@@ -29,16 +31,39 @@ public class SplashActivity extends AppCompatActivity {
         binding = ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        if (getSupportActionBar() != null) getSupportActionBar().hide();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        checkUser();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            String user_id = auth.getCurrentUser().getUid();
+            DocumentReference ref = Database.DOC_PROFILE(user_id);
+
+            ProfileViewModel vm = new ViewModelProvider(this).get(ProfileViewModel.class);
+            vm.getData().observe(this, this::profileListener);
+            vm.start(ref);
+
+            showLoading();
+
+        } else {
+            animate();
+        }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        ProfileViewModel vm = new ViewModelProvider(this).get(ProfileViewModel.class);
+        vm.getData().removeObservers(this);
+    }
 
     public void onClickSignIn(View view) {
         Interaction.next(this, LoginActivity.class);
@@ -48,42 +73,28 @@ public class SplashActivity extends AppCompatActivity {
         Interaction.next(this, RegisterActivity.class);
     }
 
+    private void profileListener(ProfileModel data) {
+
+        dialog.dismiss();
+    }
+
     private void animate() {
         binding.title.setAnimation(AnimationUtils.loadAnimation(this, R.anim.in_from_top));
         binding.subtitle.setAnimation(AnimationUtils.loadAnimation(this, R.anim.in_from_top));
         binding.container.setAnimation(AnimationUtils.loadAnimation(this, R.anim.in_from_bottom));
     }
 
-    private void checkUser() {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
+    private void showLoading() {
+        dialog = new CatLoadingView();
+        dialog.setCancelable(false);
+        dialog.show(getSupportFragmentManager(), "");
 
-        if (auth.getCurrentUser() != null) {
-            dialog = new CatLoadingView();
-            dialog.setCancelable(false);
-            dialog.show(getSupportFragmentManager(), "");
-
-            savedUser(Database.getUser());
-
-        } else animate();
+        savedUser(Database.getUser());
     }
 
     private void savedUser(FirebaseUser user) {
         SharedPreferences.Editor editor = getSharedPreferences(Interaction.APPLICATION_NAME, Context.MODE_PRIVATE).edit();
         editor.putString(Interaction.SHARED_SAVED_USER_EMAIL, user.getEmail());
         editor.apply();
-    }
-
-    private void executeAuthType(ProfileModel data) {
-        Intent intent;
-
-        if (data != null) {
-            if (data.getStaff()) intent = new Intent(this, MainAdminActivity.class);
-            else intent = new Intent(this, MainActivity.class);
-        } else {
-            intent = new Intent(this, ProfileActivity.class);
-            intent.putExtra(Interaction.EXTRA_BOOLEAN_PROFILE_EXIST, false);
-        }
-
-        Interaction.nextEnd(this, intent);
     }
 }
